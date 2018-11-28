c1de30d4fbcf9472114427566b8d574feee72f22
zakariaahmed <zackyahmed89@gmail.com>
2018-11-28 13:53:57 +0100

    update

--- settings.js
+++ settings.js
@@ -2,10 +2,12 @@
 
 const DEV_DB_URI = "mongodb://localhost:27017/userDatabase";
 const TEST_DB_URI = "mongodb://localhost:27017/testUserDatabase";
+const MLAB_URI = "mongodb://zakariaahmed:Z27546827z@ds115244.mlab.com:15244/miniproject3";
 const MOCHA_TEST_TIMEOUT = 5000;
 
 module.exports = {
   DEV_DB_URI,
   TEST_DB_URI,
-  MOCHA_TEST_TIMEOUT
+  MOCHA_TEST_TIMEOUT,
+  MLAB_URI
 }
